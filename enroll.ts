d74676dd2b80d13befd07cf4c756fce2e6505e21
Hemant <hrsmart2150@gmail.com>
2024-05-10 00:03:58 +0530

    Solana Cohorot Q2 Airdrop Assignment

---
+++ enroll.ts
@@ -0,0 +1,42 @@
+import { AnchorProvider, Program, Wallet } from "@coral-xyz/anchor";
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import base58 from "bs58";
+import { IDL, WbaPrereq } from "./program/wba_prereq";
+import wallet from "./wba-wallet.json";
+//converting the private key string to base58
+const walletbase58 = base58.decode(wallet);
+//getting the keypair
+const keypair = Keypair.fromSecretKey(new Uint8Array(walletbase58));
+//establishing the connection
+const connection = new Connection("https://api.devnet.solana.com");
+//Github account
+const github = Buffer.from("hemantwasthere", "utf-8");
+
+const provider = new AnchorProvider(connection, new Wallet(keypair));
+
+const program = new Program<WbaPrereq>(IDL, provider);
+
+// creating the PDA of our enrollment account
+
+const enrollment_seeds = [Buffer.from("prereq"), keypair.publicKey.toBuffer()];
+const [enrollment_key, _bump] = PublicKey.findProgramAddressSync(
+  enrollment_seeds,
+  program.programId
+);
+
+(async () => {
+  try {
+    const txHash = await program.methods
+      .complete(github)
+      .accounts({
+        signer: keypair.publicKey,
+      })
+      .signers([keypair])
+      .rpc();
+    console.log(
+      `Success! Check out your TX here: https://explorer.solana.com/tx/${txHash}?cluster=devnet`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})();
